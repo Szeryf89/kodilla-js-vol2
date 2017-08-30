9ce56144563f2882fe3793b93fb1c28b90d973a0
marcni <marcin.radomski89@gmail.com>
2017-08-30 20:21:38 +0200

    add comments to consol.log

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,11 @@ function getTriangleArea(a, h) {
 };
 
 function randomNumber() {
-
     return Math.floor(Math.random() * 10) + 1;
 };
 
 var a = randomNumber();
 var h = randomNumber();
 
-console.log( getTriangleArea(6, 10) );
-console.log( getTriangleArea(a, h) );
+console.log(('a = 6 and h = 10 result = ') +  getTriangleArea(6, 10) );
+console.log(('result from random a and h = ') + getTriangleArea(a, h) );
