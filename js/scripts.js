b8768ed8e60b426e3ca768bd5ded4b4b1dffd00a
marcni <marcin.radomski89@gmail.com>
2017-08-30 20:44:40 +0200

    added task from module 9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//function
+//Module 9.1
 
 function getTriangleArea(a, h) {
 
@@ -13,8 +13,31 @@ function randomNumber() {
     return Math.floor(Math.random() * 10) + 1;
 };
 
+
 var a = randomNumber();
 var h = randomNumber();
 
 console.log(('a = 6 and h = 10 result = ') +  getTriangleArea(6, 10) );
 console.log(('result from random a and h = ') + getTriangleArea(a, h) );
+
+
+//Module 9.2
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(femaleNames);
+console.log(maleNames);
+console.log(allNames);
+
+var newName = 'Marian'
+    if (allNames.indexOf(newName) === -1) {
+        console.log(allNames.push(newName));
+    }
+
+console.log(allNames);
+
+//Module 9.3
+
